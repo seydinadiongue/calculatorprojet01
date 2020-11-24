4ba3aa1ee42349997f6c10161db38f3a3606280f
seydinaoumar <seydinaoumardiongue@gmail.com>
2020-11-24 02:12:31 +0100

    seydina

--- src/test/java/calculor/calculatorTest.java
+++ src/test/java/calculor/calculatorTest.java
@@ -21,57 +21,10 @@ public class calculatorTest {
 
 
     }
-
     @Test
     public void minus() {
 
         Assert.assertEquals(0, calculator.minus(2,2));
     }
 
-    @Test
-    public void multiply() {
-        Assert.assertEquals(4, calculator.multiply(2,2));
     }
-
-    @Test
-    public void divide() {
-         int result= (int) calculator.divide(6,3);
-         Assert.assertEquals(2,result);
-    }
-   // @Test(expected = ArithmeticException.class)
-  //  public void divideWiththrowExeption() {
-   //     calculator.divide(6,3);
-   // }
-
-    @Test
-    public void min() {
-        int min=calculator.min(10,20);
-        if (min !=10) Assert.fail();
-    }
-
-    @Test
-    public void max() {
-         int max=calculator.max(2000,-2000);
-         Assert.assertEquals(2000,max);
-       
-    }
-
-    @Test
-    public void minElement() {
-        int tab[]={
-                2,4,6,7
-        };
-        int a=calculator.minElement(tab);
-        Assert.assertEquals(2,a);
-
-
-}
-    @Test
-    public void maxElement() {
-        int tab[]={
-                2,4,6,7
-        };
-        int a=calculator.maxElement(tab);
-        Assert.assertEquals(7,a);
-
-}}
